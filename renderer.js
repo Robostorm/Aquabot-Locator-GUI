bac7be63aebf605d240819af04ded30bbd0f3b3d
Tim <timothyhollabaugh@gmail.com>
2017-08-14 20:45:43 -0400

    Can choose serial port and scales image

--- renderer.js
+++ renderer.js
@@ -7,6 +7,8 @@ SerialPort = require("serialport");
 console.log("HELLO");
 
 var canvas = document.getElementById("fair_image");
+var portselect = document.getElementById("ports")
+console.log(ports)
 var image = new Image();
 
 var ctx = canvas.getContext("2d");
@@ -29,63 +31,111 @@ var end_y = 831;
 // Tim's House
 image.src = 'map.png';
 
-var start_lat  = 40.672233;
+var start_lat = 40.672233;
 var start_long = -74.841474;
-var end_lat    = 40.671406;
-var end_long   = -74.839740;
+var end_lat = 40.671406;
+var end_long = -74.839740;
 
 var start_x = 0;
 var start_y = 0;
-var end_x   = 955;
-var end_y   = 598;
+var end_x = 955;
+var end_y = 598;
 
+var scale = 1;
 
 var long_dist = start_long - end_long;
-var lat_dist  = start_lat - end_lat;
+var lat_dist = start_lat - end_lat;
 
 var dist_x = start_x - end_x;
 var dist_y = start_y - end_y;
 
-var aquabot_lat  = 40.671547;
+var aquabot_lat = 40.671547;
 var aquabot_long = -74.840057;
 
-var port = new SerialPort("/dev/usbtop", {
-  baudRate: 9600,
-  parser: SerialPort.parsers.readline('\n')
-});
+// Clear the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.drawImage(image, 0, 0);
 
-port.on('data', function (data) {
-  console.log('Data: ' + data);
-  var parsed = data.split(":");
-  console.log(parsed);
-  aquabot_lat = parsed[3];
-  console.log(aquabot_lat);
-  aquabot_long = parsed[4];
-  console.log(aquabot_long);
-});
+function resize() {
+  var img_ratio = image.height / image.width;
+  var screen_ratio = window.innerHeight / window.innerWidth;
 
-console.log('Starting Loop');
+  if (img_ratio > screen_ratio) {
+    scale = window.innerHeight / image.height;
+  } else {
+    scale = window.innerWidth / image.width;
+  }
+}
 
-window.setInterval(function(){
+function redraw() {
+  var aquabot_x = (((aquabot_long - start_long) / long_dist) * dist_x + start_x) * scale;
+  var aquabot_y = (((aquabot_lat - start_lat) / lat_dist) * dist_y + start_y) * scale;
 
-
-  // Test moving aquabot
-  //aquabot_long += .00001;
-  //aquabot_lat += .00001;
-
-  var aquabot_x = ((aquabot_long - start_long)/long_dist)*dist_x + start_x;
-  var aquabot_y = ((aquabot_lat - start_lat)/lat_dist)*dist_y + start_y;
-
-  // Clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Draw the image
-  ctx.drawImage(image, 0, 0);
+  ctx.drawImage(image, 0, 0, image.width * scale, image.height * scale);
 
   // Set the color
   ctx.fillStyle = "#0600ff";
 
   // Draw the rectangle marker
   ctx.fillRect(aquabot_x - 10, aquabot_y - 10, 20, 20);
-}, 10);
+}
+
+window.onresize = function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  resize()
+  redraw()
+}
+
+SerialPort.list(function (err, ports) {
+  ports.forEach(function (port) {
+    console.log(port);
+    console.log(portselect.options);
+    var opt = document.createElement('option');
+    opt.value = port.comName;
+    opt.innerHTML = port.comName + " => " + port.serialNumber
+    portselect.appendChild(opt)
+  });
+  updateMap();
+});
+
+var port = null;
+
+function updateMap() {
+
+  if (port !== null) {
+    port.close();
+  }
+
+  console.log("value: " + portselect.value)
+
+  port = new SerialPort(portselect.value, {
+    baudRate: 9600,
+    parser: SerialPort.parsers.readline('\n')
+  });
+
+  console.log(port)
+
+  port.on('data', function (data) {
+    console.log('Data: ' + data);
+    var parsed = data.split(":");
+    console.log(parsed);
+    aquabot_lat = parsed[3];
+    console.log(aquabot_lat);
+    aquabot_long = parsed[4];
+    console.log(aquabot_long);
+
+    // Test moving aquabot
+    //aquabot_long += .00001;
+    //aquabot_lat += .00001;
+
+    redraw();
+  });
+};
+
+portselect.onchange = updateMap;
+
 
